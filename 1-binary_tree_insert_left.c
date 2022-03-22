74846aea940481abbbd508e3634a67e231b9b1db
jhonpulido1990 <3825@holbertonschool.com>
2022-03-22 16:47:35 -0500

    add evaluation if parent is NULL and style betty

--- 1-binary_tree_insert_left.c
+++ 1-binary_tree_insert_left.c
@@ -1,34 +1,39 @@
 #include <stdlib.h>
 #include "binary_trees.h"
 /**
- * binary_tree_node - Function include a new node at left of parent
+ * binary_tree_insert_left - Function include a new node at left of parent
  * @parent: new node's parent
  * @value: new node's value
  * Return: Always 0 (Success)
  */
 binary_tree_t *binary_tree_insert_left(binary_tree_t *parent, int value)
 {
-    binary_tree_t *tempNode;
-    binary_tree_t *parent_n = parent;
-    binary_tree_t *tempChild = NULL;
-    
-    tempNode = malloc(sizeof(binary_tree_t));
-    tempNode->n = value;
-    tempNode->left = NULL;
-    tempNode->right = NULL;
-    
-    if (parent_n->left == NULL)
-    {
-      parent_n->left = tempNode;
-      tempNode->parent = parent_n;
-      return parent_n;
-    } else
-    {
-      tempChild = parent_n->left;
-      parent_n->left = tempNode;
-      tempNode->parent = parent_n;
-      tempNode->left = tempChild;
-      tempChild->parent = tempNode;
-      return parent_n;
-    }
+	binary_tree_t *tempNode;
+	binary_tree_t *parent_n = parent;
+	binary_tree_t *tempChild = NULL;
+
+	if (parent == NULL)
+		return (NULL);
+	tempNode = malloc(sizeof(binary_tree_t));
+	if (tempNode == NULL)
+		return (NULL);
+	tempNode->n = value;
+	tempNode->left = NULL;
+	tempNode->right = NULL;
+
+	if (parent_n->left == NULL)
+	{
+		parent_n->left = tempNode;
+		tempNode->parent = parent_n;
+		return (parent_n);
+	}
+	else
+	{
+		tempChild = parent_n->left;
+		parent_n->left = tempNode;
+		tempNode->parent = parent_n;
+		tempNode->left = tempChild;
+		tempChild->parent = tempNode;
+		return (parent_n);
+	}
 }
