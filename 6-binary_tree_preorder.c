62267007d43bc50f58b6ced3162b3a57da6bc0c7
easantanders21 <3823@holbertonschool.com>
2022-03-23 10:42:15 -0500

    fix task 6

--- 6-binary_tree_preorder.c
+++ 6-binary_tree_preorder.c
@@ -8,8 +8,6 @@
  */
 void binary_tree_preorder(const binary_tree_t *tree, void (*func)(int))
 {
-	if (tree == NULL)
-		return;
 	func(tree->n);
 	/*Left sub-tree */
 	binary_tree_preorder(tree->left, func);
