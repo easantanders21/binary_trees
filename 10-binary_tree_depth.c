ee7220f0427b425cbb7e3a544448200a059faa11
jhonpulido1990 <3825@holbertonschool.com>
2022-03-23 15:16:45 -0500

    atyle betty

--- 10-binary_tree_depth.c
+++ 10-binary_tree_depth.c
@@ -8,11 +8,11 @@
  */
 size_t binary_tree_depth(const binary_tree_t *tree)
 {
-    int suma = 0;
+	int suma = 0;
 
-    if (tree->parent == NULL)
-        return (0);
-    suma = binary_tree_depth(tree->parent) + 1;
+	if (tree->parent == NULL)
+		return (0);
+	suma = binary_tree_depth(tree->parent) + 1;
 
-    return (suma);
+	return (suma);
 }
