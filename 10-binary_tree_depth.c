bd5a39575d8cc30af834d5e7591ff23b4a38127b
easantanders21 <3823@holbertonschool.com>
2022-03-23 15:13:40 -0500

    task 10

---
+++ 10-binary_tree_depth.c
@@ -0,0 +1,18 @@
+#include <stdlib.h>
+#include "binary_trees.h"
+/**
+ * binary_tree_depth - find depth by node
+ *
+ * @tree: binary tree
+ * Return: depth node
+ */
+size_t binary_tree_depth(const binary_tree_t *tree)
+{
+    int suma = 0;
+
+    if (tree->parent == NULL)
+        return (0);
+    suma = binary_tree_depth(tree->parent) + 1;
+
+    return (suma);
+}
