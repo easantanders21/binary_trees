f75b3d7adec6eafa4a370031bc6580a4c10d441d
easantanders21 <3823@holbertonschool.com>
2022-03-23 11:31:32 -0500

    task 9 fix

--- 9-binary_tree_height.c
+++ 9-binary_tree_height.c
@@ -1,6 +1,11 @@
 #include <stdlib.h>
 #include "binary_trees.h"
-
+/**
+ * binary_tree_height - binary tree using post-order traversal
+ *
+ * @tree: binary tree
+ * Return: That measures the height of a binary tree
+ */
 size_t binary_tree_height(const binary_tree_t *tree)
 {
 	int sum1 = 0, sum2 = 0, total = 0;
@@ -16,9 +21,13 @@ size_t binary_tree_height(const binary_tree_t *tree)
 	return (total);
 }
 /**
- * Find maximum between two numbers.
+ * max - binary tree using post-order traversal
+ *
+ * @num1: binary tree
+ * @num2: binary tree
+ * Return: max between num1 and num2
  */
 int max(int num1, int num2)
 {
-    return (num1 > num2 ) ? num1 : num2;
+	return (num1 > num2 ) ? num1 : num2;
 }
