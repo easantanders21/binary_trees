843efe8285cc4604f547ac8cbe0df62b48822750
jhonpulido1990 <3825@holbertonschool.com>
2022-03-23 23:23:40 -0500

    if tree->left and tree->rigth equal NULL

--- 15-binary_tree_is_full.c
+++ 15-binary_tree_is_full.c
@@ -10,7 +10,7 @@ int binary_tree_is_full(const binary_tree_t *tree)
 {
 	if (tree == NULL)
 		return (0);
-	if (tree->left != NULL && tree->right != NULL && tree->parent != NULL)
+	if (tree->left == NULL && tree->right == NULL)
 		return (1);
 	if ((tree->left) && (tree->right))
 		return (binary_tree_is_full(tree->left) &&
