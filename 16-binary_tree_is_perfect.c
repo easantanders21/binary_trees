905e6e6d92a361cbd18a2b65744fdb862a524a63
easantanders21 <3823@holbertonschool.com>
2022-03-24 10:48:07 -0500

    fix task 16

--- 16-binary_tree_is_perfect.c
+++ 16-binary_tree_is_perfect.c
@@ -15,7 +15,7 @@ int binary_tree_is_perfect(const binary_tree_t *tree)
 		return (0);
 
 	if (tree->left == NULL && tree->right == NULL)
-		return (1);
+		return (0);
 
 	balance = binary_tree_balance(tree);
 	full = binary_tree_is_full(tree);
